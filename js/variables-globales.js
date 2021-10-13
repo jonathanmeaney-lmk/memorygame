7d8cf5b1fb0c59ff53bf96a56edac525cffd03df
Jonathan <jonathanmeaney1982@gmail.com>
2021-10-13 12:19:47 +0000

    Unit 6 complete

--- js/variables-globales.js
+++ js/variables-globales.js
@@ -1,3 +1,18 @@
-var grupoTarjetas = ["🦄", "🍦", "🌈", "👽", "👾", "🤖", "👹", "👺"];
+var movimientos = 0;
+var grupoTarjetas = [["🦄", "🍦"], ["🌈", "👽"], ["👾", "🤖"]];
+var nivelActual = 0;
+var niveles = [
+    {
+        tarjetas: grupoTarjetas[0],
+        movimientosMax: 3
+    },
+    {
+        tarjetas: grupoTarjetas[0].concat(grupoTarjetas[1]),
+        movimientosMax: 8
+    },
+    {
+        tarjetas: grupoTarjetas[0].concat(grupoTarjetas[1], grupoTarjetas[2]),
+        movimientosMax: 12
+    }
+];
 
-var totalTarjetas = grupoTarjetas.concat(grupoTarjetas);
